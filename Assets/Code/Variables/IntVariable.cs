d72cc548756981ce0f4365722347acbff2dc9c14
Ray Millares <rmill013@gmail.com>
2018-08-21 18:27:45 -0400

    Original Highscore commit

---
+++ Assets/Code/Variables/IntVariable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Vars/IntVariable")]
+public class IntVariable : ScriptableObject
+{
+    public float Value;
+}
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader : MonoBehaviour {
+public class LevelLoader : MonoBehaviour
+{
 
-	public void LoadLevel (string sceneName)
+    bool b_canStart = false;
+    AsyncOperation asyncLoad;
+
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAsync("Prototype01"));
+    }
+
+    private void Update()
+    {
+        if (asyncLoad != null)
+            print("Progress: " + asyncLoad.progress);
+    }
+
+    public void LoadLevel (string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public IEnumerator LoadLevelAsync (string sceneName)
+    {
+       asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+       asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 
     public void QuitGame ()
     {
         Application.Quit();
         Debug.Log("Player Quit Game");
+    }
+
+    public void AllowSceneActivation ()
+    {
+        if (asyncLoad != null)
+            asyncLoad.allowSceneActivation = true;
     }
 }
